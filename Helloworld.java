59115a75ed3865233f73f41ba505e79068443e6e
Sibisarath <60036932+Sibisarath@users.noreply.github.com>
2022-08-18 22:22:13 +0530

    Added Addition

--- Helloworld.java
+++ Helloworld.java
@@ -5,5 +5,10 @@ public static void main(String[]args)
 {
   System.out.println("Hello World");
   System.out.println("Greetings");
+  System.out.println("Addition");
+  int a=10;
+  int b=20;
+  int sum=a+b;
+  System.out.println(sum);
 }
 }
